2cb9b03b930ead1a990374ab79cd1093c23fdc86
Switham007 <sam.switham@gmail.com>
2020-06-29 10:04:46 +0100

    committed last weeks changes where Choose_guides.ipynb was edited

--- src/data_sorting/OpenChromatin_coverage.sh
+++ src/data_sorting/OpenChromatin_coverage.sh
@@ -12,3 +12,8 @@ xpref=${xbase%.*}
 coverageBed -a $1 -b ../../data/ATAC-seq/potter2018/Roots_NaOH_peaks_all.bed > ../../data/output/$2/chromatin_coverage/${xpref}RootOpenChrom.bp_covered.txt
 coverageBed -a $1 -b ../../data/ATAC-seq/potter2018/Shoots_NaOH_peaks_all.bed > ../../data/output/$2/chromatin_coverage/${xpref}ShootOpenChrom.bp_covered.txt
 coverageBed -a $1 -b ../../data/ATAC-seq/potter2018/intersectRootsShoots_PeaksInBoth.bed > ../../data/output/$2/chromatin_coverage/${xpref}ShootRootIntersectOpenChrom.bp_covered.txt
+
+#Also run bedtools intersect and output files to the same location (not currently used by any subsequent scripts)
+intersectBed -wo -a $1 -b ../../data/ATAC-seq/potter2018/Roots_NaOH_peaks_all.bed > ../../data/output/$2/chromatin_coverage/${xpref}RootOpenChrom.intersect.txt
+intersectBed -wo -a $1 -b ../../data/ATAC-seq/potter2018/Shoots_NaOH_peaks_all.bed > ../../data/output/$2/chromatin_coverage/${xpref}ShootOpenChrom.intersect.txt
+intersectBed -wo -a $1 -b ../../data/ATAC-seq/potter2018/intersectRootsShoots_PeaksInBoth.bed > ../../data/output/$2/chromatin_coverage/${xpref}ShootRootIntersectOpenChrom.intersect.txt
